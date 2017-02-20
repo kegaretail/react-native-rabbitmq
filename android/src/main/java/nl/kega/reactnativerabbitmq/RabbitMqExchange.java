06aa61c46684a9d59c834a7fdcf77983d45fe0cf
Tim Honders <tim.honders@kega.nl>
2017-02-20 15:33:37 +0100

    Android poperties type checking

--- android/src/main/java/nl/kega/reactnativerabbitmq/RabbitMqExchange.java
+++ android/src/main/java/nl/kega/reactnativerabbitmq/RabbitMqExchange.java
@@ -8,6 +8,7 @@ import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.bridge.ReadableType;
 import com.facebook.react.modules.core.DeviceEventManagerModule;
 
 import com.rabbitmq.client.Channel;
@@ -55,20 +56,49 @@ public class RabbitMqExchange {
             AMQP.BasicProperties.Builder properties = new AMQP.BasicProperties.Builder();
 
             if (message_properties != null){
-                if (message_properties.hasKey("content_type")){properties.contentType(message_properties.getString("content_type"));}
-                if (message_properties.hasKey("content_encoding")){properties.contentEncoding(message_properties.getString("content_encoding"));}
-                if (message_properties.hasKey("delivery_mode")){properties.deliveryMode(message_properties.getInt("delivery_mode"));}
-                if (message_properties.hasKey("priority")){properties.priority(message_properties.getInt("priority"));}
-                if (message_properties.hasKey("correlation_id")){properties.correlationId(message_properties.getString("correlation_id"));}
-                if (message_properties.hasKey("expiration")){ properties.expiration(message_properties.getString("expiration"));}
-                if (message_properties.hasKey("message_id")){properties.messageId(message_properties.getString("message_id"));}
-                if (message_properties.hasKey("type")){properties.type(message_properties.getString("type"));}
-                if (message_properties.hasKey("user_id")){properties.userId(message_properties.getString("user_id"));}
-                if (message_properties.hasKey("app_id")){properties.appId(message_properties.getString("app_id"));}
-                if (message_properties.hasKey("reply_to")){properties.replyTo(message_properties.getString("reply_to"));}
+                 try {
+                     
+                    if (message_properties.hasKey("content_type") && message_properties.getType("content_type") == ReadableType.String){
+                        properties.contentType(message_properties.getString("content_type"));
+                    }
+                    if (message_properties.hasKey("content_encoding") && message_properties.getType("content_encoding") == ReadableType.String){
+                        properties.contentEncoding(message_properties.getString("content_encoding"));
+                    }
+                    if (message_properties.hasKey("delivery_mode") && message_properties.getType("delivery_mode") == ReadableType.Number){
+                        properties.deliveryMode(message_properties.getInt("delivery_mode"));
+                    }
+                    if (message_properties.hasKey("priority") && message_properties.getType("priority") == ReadableType.Number){
+                        properties.priority(message_properties.getInt("priority"));
+                    }
+                    if (message_properties.hasKey("correlation_id") && message_properties.getType("correlation_id") == ReadableType.String){
+                        properties.correlationId(message_properties.getString("correlation_id"));
+                    }
+                    if (message_properties.hasKey("expiration") && message_properties.getType("correlation_id") == ReadableType.String){ 
+                        properties.expiration(message_properties.getString("expiration"));
+                    }
+                    if (message_properties.hasKey("message_id") && message_properties.getType("message_id") == ReadableType.String){
+                        properties.messageId(message_properties.getString("message_id"));
+                    }
+                    if (message_properties.hasKey("type") && message_properties.getType("type") == ReadableType.String){
+                        properties.type(message_properties.getString("type"));
+                    }
+                    if (message_properties.hasKey("user_id") && message_properties.getType("user_id") == ReadableType.String){
+                        properties.userId(message_properties.getString("user_id"));
+                    }
+                    if (message_properties.hasKey("app_id") && message_properties.getType("app_id") == ReadableType.String){
+                        properties.appId(message_properties.getString("app_id"));
+                    }
+                    if (message_properties.hasKey("reply_to") && message_properties.getType("reply_to") == ReadableType.String){
+                        properties.replyTo(message_properties.getString("reply_to"));
+                    }
 
-                //if (message_properties.hasKey("timestamp")){properties.timestamp(message_properties.getBoolean("timestamp"));}
-                //if (message_properties.hasKey("headers")){properties.expiration(message_properties.getBoolean("headers"))}
+                    //if (message_properties.hasKey("timestamp")){properties.timestamp(message_properties.getBoolean("timestamp"));}
+                    //if (message_properties.hasKey("headers")){properties.expiration(message_properties.getBoolean("headers"))}
+
+                 } catch (Exception e){
+                    Log.e("RabbitMqExchange", "Exchange publish properties error " + e);
+                    e.printStackTrace();
+                }
             }
             
             this.channel.basicPublish(this.name, routing_key, properties.build(), message_body_bytes);
